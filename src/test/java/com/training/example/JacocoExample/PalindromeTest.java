a18c70a1e2452210d0f6e3bcb8815da1204ae74d
jalandhar44 <jalandharmandra98@gmail.com>
2024-10-23 15:25:27 +0530

    Update PalindromeTest.java

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -11,4 +11,9 @@ public class PalindromeTest {
 	    Palindrome palindromeTester = new Palindrome();
 	    assertTrue(palindromeTester.isPalindrome("madam"));
 	}
+	@Test
+	public void whenEmptyString_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("malayalam"));
+	}
 }
